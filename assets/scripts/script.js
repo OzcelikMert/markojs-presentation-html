72c039cb0e78627881315d910d329731d134686b
Mert ÖZÇELİK <mert.ozcelikk@hotmail.com>
2023-04-12 19:19:50 +0300

    qweqweqwe

---
+++ assets/scripts/script.js
@@ -0,0 +1,31 @@
+/*jslint browser:true */
+$(document).ready(function () {
+	var $navbar = $('.navbar-default');
+	var $offsetY = 750;
+	var $scrollButton = $('.scroll');
+	var $navbarLink = $('.navbar-nav .nav-link');
+
+	// Fixed Nav after scroll
+	function scroll() {
+		if ($(window).scrollTop() >= $offsetY) {
+			$navbar.addClass('fixed-top');
+		} else {
+			$navbar.removeClass('fixed-top');
+		}
+	}
+	document.onscroll = scroll;
+
+	// Smooth scroll to content
+	$scrollButton.on('click', function (e) {
+		e.preventDefault();
+		var $link = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $($link).offset().top - 60
+		}, 100);
+	});
+
+	$navbarLink.on("click", function(e) {
+		$navbarLink.removeClass("active");
+		$(this).addClass("active");
+	})
+});
